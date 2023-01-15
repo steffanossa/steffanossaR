a283c4075f1cb7a4f6aada9a55dc38f2489ffba9
ss395739 <ss395739@fh-muenster.de>
2023-01-15 14:11:48 +0100

    update

---
+++ R/registerPar.R
@@ -0,0 +1,20 @@
+#' Register cores for parallel processing on Unix & Windows
+#' @note just a wrap for unix and windows systems. DoParallel should theoretically work on both, though. Will leave 1 core for os if not run on r-workbench.
+#' @export
+registerPar <- function() {
+  os_type <- Sys.info()["sysname"]
+  if (os_type == "Windows") {
+    n_cores <- parallel::detectCores(logical = F) - 1
+    DoParallel::registerDoParallel(makePSOCKcluster(n_cores))
+  } else {
+    if (Sys.info()["nodename"] == "r-workbench") {
+      n_cores <- parallel::detectCores()
+      DoMC::registerDoMC(n_cores)
+    } else {
+      #' see above
+      n_cores <- parallel::detectCores() - 1
+      DoMC::registerDoMC(n_cores - 1)
+    }
+  }
+  cat(crayon::green(n_cores, "cores registered for parallel processing on", os_type))
+}
